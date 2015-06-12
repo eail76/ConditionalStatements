655fbd40f15afab30859cc994a7f8c79a2ce5d03
eail76 <eail@modelo.edu.mx>
2015-06-12 09:00:58 -0500

    Tarea 3 ConditionalStatements

---
+++ ConditionalStatements.playground/Contents.swift
@@ -0,0 +1,12 @@
+var fruit = "apples"
+var myfruit = "banana"
+
+switch fruit{
+    case "apples":
+        println("The fruit was some \(fruit)")
+    
+    case "bananas":
+        println("The fruit was a banana")
+    default:
+        println("No fruits matched")
+}
